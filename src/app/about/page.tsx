180f4133f1933dcc5ab5d844ae8c69571e9a26cf
Hasham ul haque <islamhasham11@gmail.com>
2025-01-12 10:14:28 +0500

    updated

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,7 +6,7 @@ const About = () => {
       <section className='about' style={{backgroundImage:"url(watchbackground.jpg)"}}>
         <div className='about-content'>
             <h2>ABOUT US</h2>
-            <p>At WATCH MANIA, we are passionate about delivering more than just watches  we offer a symbol of elegance, precision, and individuality. Our carefully curated collection features a range of designs to suit every personality, from timeless classics to contemporary masterpieces. With a commitment to quality craftsmanship and exceptional customer service, we aim to provide an experience that goes beyond timekeeping. Whether you're marking a special occasion or enhancing your everyday style, we are here to help you find the perfect piece to cherish forever.</p>
+            <p>At WATCH MANIA we are passionate about delivering more than just watches  we offer a symbol of elegance precision and individuality. Our carefully curated collection features a range of designs to suit every personality from timeless classics to contemporary masterpieces. With a commitment to quality craftsmanship and exceptional customer service we aim to provide an experience that goes beyond timekeeping. Whether youre marking a special occasion or enhancing your everyday style we are here to help you find the perfect piece to cherish forever.</p>
 
         </div>
       </section>
